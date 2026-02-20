25b6e89ba320b45b26bb5ac6f8dfbbd46b1c0679
nok-g <znihei612@gmail.com>
2026-02-20 13:37:29 +0900

    initial commit

---
+++ ワープのオンとオフ.swift
@@ -0,0 +1,24 @@
+func set() {
+    moveForward()
+        if isOnGem {
+            collectGem()
+        }
+        if isBlocked {
+            turnLeft()
+            turnLeft()
+        }
+}
+for i in 1...9 {
+    set()
+}
+purplePortal.isActive = true
+moveForward()
+turnLeft()
+turnLeft()
+purplePortal.isActive = false
+
+for i in 1...7 {
+    set()
+}
+moveForward()
+toggleSwitch()
